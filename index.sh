578500dd47e9ed520ab02a90149d919fe29fb68b
Braden Polly <bpolly@covermymeds.com>
2019-08-21 20:18:30 -0400

    Get rid of case statement

--- index.sh
+++ index.sh
@@ -1,42 +1,25 @@
 #!/bin/sh
-# set -x
 
+# Number of branches to list
+branch_count=5
+
+# Fetch branch names
 git_branches(){
-    git for-each-ref --count=5 --sort=-committerdate refs/heads/ --format='%(refname:short)'
+    git for-each-ref --count=$branch_count --sort=-committerdate refs/heads/ --format='%(refname:short)'
 }
 
 title="=== Most Recent Git Branches ==="
 prompt="Choose a branch:"
 echo "$title"
 PS3="$prompt "
-select opt in $(git_branches) "Quit"; do 
+select opt in $(git_branches); do 
 
-    case "$REPLY" in
-
-    1 )
-        # echo "You picked $opt which is option $REPLY";
+    if [ "$REPLY" -ge 1 ] && [ "$REPLY" -le $branch_count ]; then
         (git checkout "$opt");
-        break;;
-    2 ) 
-        # echo "You picked $opt which is option $REPLY";
-        (git checkout "$opt");
-        break;;
-    3 ) 
-        (git checkout "$opt");
-        break;;
-    4 ) 
-        (git checkout "$opt");
-        break;;
-    5 ) 
-        (git checkout "$opt");
-        break;;
-
-    $(( ${#options[@]}+1 )) ) 
-        echo "Goodbye!"; break;;
-    *) 
-        echo "Invalid option. Try another one.";continue;;
-
-    esac
-
+        break
+    else
+        echo "Invalid option.";
+        continue
+    fi
 done
 
